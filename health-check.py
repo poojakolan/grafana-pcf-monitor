8b6b993caedb99a4d0fc3c2b2fdc0649b5a9dc07
poojakolan <lakshmipoojak@gmail.com>
2018-11-05 23:11:04 +0530

    Create health-check.py

---
+++ health-check.py
@@ -0,0 +1,34 @@
+import subprocess
+import shlex
+import json
+import datetime
+import time
+import xlsxwriter
+import httplib, urllib
+import math
+import bitmath
+import mysql.connector
+
+timestamp_str = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d-%H:%M:%S')
+health_watch_url = "healthwatch.hdc2.cloud.8451.com"
+
+db = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="root",
+  database="grafana"
+)
+
+mycursor = db.cursor()
+#mycursor.execute(update_app_historic, ("H",))
+#mycursor.execute(update_foundry_historic, ("H",))
+#db.commit()
+params = urllib.urlencode({})
+headers = {}
+conn = httplib.HTTPSConnection(health_watch_url)
+conn.request("GET", "/panels/diego", params, headers)
+response = conn.getresponse()
+print(response.status, response.reason)
+data = response.read()
+conn.close()
+json_stats = json.loads(data)
