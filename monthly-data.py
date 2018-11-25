aa51a992affce415b3662ef3bdeba84cdf0c907e
poojakolan <lakshmipoojak@gmail.com>
2018-11-26 00:19:18 +0530

    Create monthly-data.py

---
+++ monthly-data.py
@@ -0,0 +1,59 @@
+max concurrent:
+
+
+
+max apps and tasks 
+
+
+
+url: https://apps.hdc2.cloud.8451.com/proxy/accounting_report/system_report/max_apps_and_tasks
+
+
+
+response: {"report_time":"2018-11-25 05:55:53 UTC","monthly_reports":[{"year":2018,"month":4,"maximum_concurrent_apps_and_tasks":22},{"year":2018,"month":5,"maximum_concurrent_apps_and_tasks":69},{"year":2018,"month":6,"maximum_concurrent_apps_and_tasks":72},{"year":2018,"month":7,"maximum_concurrent_apps_and_tasks":81},{"year":2018,"month":8,"maximum_concurrent_apps_and_tasks":94},{"year":2018,"month":9,"maximum_concurrent_apps_and_tasks":93},{"year":2018,"month":10,"maximum_concurrent_apps_and_tasks":99},{"year":2018,"month":11,"maximum_concurrent_apps_and_tasks":79}],"yearly_reports":[{"year":2018,"maximum_concurrent_apps_and_tasks":99}]}
+
+
+
+------------------------------------
+
+
+
+tasks:
+
+
+
+task usages api
+
+
+
+url: https://apps.hdc2.cloud.8451.com/proxy/accounting_report/system_report/task_usages
+
+
+
+response: {"report_time":"2018-11-25T05:55:53.504Z","monthly_reports":[{"month":3,"year":2018,"total_task_runs":0,"maximum_concurrent_tasks":0,"task_hours":0.0},{"month":4,"year":2018,"total_task_runs":0,"maximum_concurrent_tasks":0,"task_hours":0.0},{"month":5,"year":2018,"total_task_runs":0,"maximum_concurrent_tasks":0,"task_hours":0.0},{"month":6,"year":2018,"total_task_runs":0,"maximum_concurrent_tasks":0,"task_hours":0.0},{"month":7,"year":2018,"total_task_runs":0,"maximum_concurrent_tasks":0,"task_hours":0.0},{"month":8,"year":2018,"total_task_runs":0,"maximum_concurrent_tasks":0,"task_hours":0.0},{"month":9,"year":2018,"total_task_runs":0,"maximum_concurrent_tasks":0,"task_hours":0.0},{"month":10,"year":2018,"total_task_runs":0,"maximum_concurrent_tasks":0,"task_hours":0.0},{"month":11,"year":2018,"total_task_runs":0,"maximum_concurrent_tasks":0,"task_hours":0.0}],"yearly_reports":[{"year":2018,"total_task_runs":0,"maximum_concurrent_tasks":0,"task_hours":0.0}]}
+
+
+
+---------------------------------------------------
+
+
+
+app usages api
+
+
+
+url: https://apps.hdc2.cloud.8451.com/proxy/accounting_report/system_report/app_usages
+
+
+
+from this url we are getting data of maax apps and apps instance hours and apps data is also same as app instance hours
+
+
+
+response: {"report_time":"2018-11-25 05:57:23 UTC","monthly_reports":[{"month":4,"year":2018,"average_app_instances":1.83496,"maximum_app_instances":22,"app_instance_hours":1321.17},{"month":5,"year":2018,"average_app_instances":43.0469,"maximum_app_instances":69,"app_instance_hours":32026.9},{"month":6,"year":2018,"average_app_instances":60.8405,"maximum_app_instances":72,"app_instance_hours":43805.1},{"month":7,"year":2018,"average_app_instances":68.3266,"maximum_app_instances":81,"app_instance_hours":50835.0},{"month":8,"year":2018,"average_app_instances":78.8466,"maximum_app_instances":94,"app_instance_hours":58661.9},{"month":9,"year":2018,"average_app_instances":80.7124,"maximum_app_instances":93,"app_instance_hours":58112.9},{"month":10,"year":2018,"average_app_instances":78.6597,"maximum_app_instances":99,"app_instance_hours":58522.8},{"month":11,"year":2018,"average_app_instances":68.16945186469059,"maximum_app_instances":79,"app_instance_hours":39671.655277778}],"yearly_reports":[{"year":2018,"average_app_instances":43.5,"maximum_app_instances":99,"app_instance_hours":342957.425277778}]}
+
+
+
+
+
+-------------------------------------------------------------------
